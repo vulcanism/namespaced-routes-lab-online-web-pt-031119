fe287603163e1d0dace79f9510511a06200c25de
Savannah C <sgcorrell@gmail.com>
2019-08-21 16:03:14 +0000

    Automatically backed up by Learn

--- db/migrate/20190821160241_create_preferences.rb
+++ db/migrate/20190821160241_create_preferences.rb
@@ -1,6 +1,11 @@
 class CreatePreferences < ActiveRecord::Migration[5.0]
   def change
     create_table :preferences do |t|
+      t.string :artist_sort_order
+      t.string :song_sort_order
+      t.boolean :allow_create_artists
+      t.boolean :allow_create_songs
+
     end
   end
 end
