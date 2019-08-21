27c9e66c787cac48fec340de0c3dff0fb08eda58
Savannah C <sgcorrell@gmail.com>
2019-08-21 16:03:50 +0000

    Automatically backed up by Learn

--- db/migrate/20190821160241_create_preferences.rb
+++ db/migrate/20190821160241_create_preferences.rb
@@ -5,7 +5,8 @@ class CreatePreferences < ActiveRecord::Migration[5.0]
       t.string :song_sort_order
       t.boolean :allow_create_artists
       t.boolean :allow_create_songs
-
+      
+      t.timestamps null: false
     end
   end
 end
